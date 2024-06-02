2eba4610a2ec43cce5cf05a4a968897445ea95d6
2rage <the2rage@icloud.com>
2024-06-02 12:36:20 +0300

    Initial Commit

---
+++ alias-game/alias_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  alias_gameApp.swift
+//  alias-game
+//
+//  Created by Konstantin Yakovlev on 02.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct alias_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
