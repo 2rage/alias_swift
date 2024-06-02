c72550a97e68cb3756184f169c61c57f37684004
2rage <the2rage@icloud.com>
2024-06-02 13:00:18 +0300

    Init commit

---
+++ alias-game/GameView.swift
@@ -0,0 +1,8 @@
+//
+//  GameView.swift
+//  alias-game
+//
+//  Created by Konstantin Yakovlev on 02.06.2024.
+//
+
+import Foundation
---
+++ alias-game/ResultView.swift
@@ -0,0 +1,8 @@
+//
+//  ResultView.swift
+//  alias-game
+//
+//  Created by Konstantin Yakovlev on 02.06.2024.
+//
+
+import Foundation
